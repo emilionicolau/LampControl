088748dbe3088806f17ca3cd921fa0ec79bb9e87
Emilio <emilionicolau@gmail.com>
2016-12-02 18:14:39 +0100

    calculo do centro do ecran calculo da posicao

--- app/src/main/java/pt/ezn/apps/lampcontrol/MainActivity.java
+++ app/src/main/java/pt/ezn/apps/lampcontrol/MainActivity.java
@@ -3,7 +3,6 @@ package pt.ezn.apps.lampcontrol;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -25,22 +24,36 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout frame = (FrameLayout) findViewById(R.id.activity_main);
         Rect rectf = new Rect();
         frame.getGlobalVisibleRect(rectf);
-        float x_center = (rectf.right - rectf.left)/2;
-        float y_center = (rectf.bottom - rectf.top)/2;
+        float x_center = (rectf.right)/2;
+        float y_center = (rectf.bottom)/2;
+        int position=0;
 
         // MotionEvent object holds X-Y values
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            String text = "Center is: " + x_center + "," + y_center + "You click at x = " +
-                    event.getX() + " and y = " + event.getY();
+            if ((event.getX() <= x_center)&&(event.getY() <= y_center))
+            {position = 1;
+            }
+            if ((event.getX() > x_center)&&(event.getY() < y_center))
+            {position = 2;
+            }
+            if ((event.getX() < x_center)&&(event.getY() > y_center))
+            {position = 3;
+            }
+            if ((event.getX() >= x_center)&&(event.getY() >= y_center))
+            {position = 4;
+            }
+
+            String text = "Center is: " + x_center + ", " + y_center + "  You click at x = " +
+                    event.getX() + " and y = " + event.getY() + " Your position is: " + position;
             Toast.makeText(this, text, Toast.LENGTH_LONG).show();
         }
 
-        Log.d("WIDTH        :",String.valueOf(rectf.width()));
+        /*Log.d("WIDTH        :",String.valueOf(rectf.width()));
         Log.d("HEIGHT       :",String.valueOf(rectf.height()));
         Log.d("left         :",String.valueOf(rectf.left));
         Log.d("right        :",String.valueOf(rectf.right));
         Log.d("top          :",String.valueOf(rectf.top));
-        Log.d("bottom       :",String.valueOf(rectf.bottom));
+        Log.d("bottom       :",String.valueOf(rectf.bottom));*/
 
         return super.onTouchEvent(event);
     }
