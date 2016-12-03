a495a55ed7972347cc3c9da171390f54dc73c0c4
Emilio <emilionicolau@gmail.com>
2016-12-03 16:14:31 +0100

    toolbar

--- app/src/main/java/pt/ezn/apps/lampcontrol/MainActivity.java
+++ app/src/main/java/pt/ezn/apps/lampcontrol/MainActivity.java
@@ -3,44 +3,46 @@ package pt.ezn.apps.lampcontrol;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
-
-
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        FrameLayout frame = (FrameLayout) findViewById(R.id.activity_main);
+        FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
         Rect rectf = new Rect();
         frame.getGlobalVisibleRect(rectf);
-        float x_center = (rectf.right)/2;
-        float y_center = (rectf.bottom)/2;
-        int position=0;
+        float x_center = (rectf.right) / 2;
+        float y_center = (rectf.bottom) / 2;
+        int position = 0;
 
         // MotionEvent object holds X-Y values
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            if ((event.getX() <= x_center)&&(event.getY() <= y_center))
-            {position = 1;
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if ((event.getX() <= x_center) && (event.getY() <= y_center)) {
+                position = 1;
             }
-            if ((event.getX() > x_center)&&(event.getY() < y_center))
-            {position = 2;
+            if ((event.getX() > x_center) && (event.getY() < y_center)) {
+                position = 2;
             }
-            if ((event.getX() < x_center)&&(event.getY() > y_center))
-            {position = 3;
+            if ((event.getX() < x_center) && (event.getY() > y_center)) {
+                position = 3;
             }
-            if ((event.getX() >= x_center)&&(event.getY() >= y_center))
-            {position = 4;
+            if ((event.getX() >= x_center) && (event.getY() >= y_center)) {
+                position = 4;
             }
 
             String text = "Center is: " + x_center + ", " + y_center + "  You click at x = " +
